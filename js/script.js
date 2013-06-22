f4a82ec832e92c2e836eb0a906b013ee8e72fead
Thanh Nguyen <thanh.nguyen@seenth.at>
2013-06-22 15:23:02 -0700

    added file upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+/*jslint unparam: true */
+/*global window, $ */
+$(function () {
+    'use strict';
+    // Change this to the location of your server-side upload handler:
+    var url = (window.location.hostname === 'blueimp.github.io' ||
+                window.location.hostname === 'blueimp.github.io') ?
+                '//jquery-file-upload.appspot.com/' : 'server/php/';
+    $('#fileupload').fileupload({
+        url: url,
+        dataType: 'json',
+        done: function (e, data) {
+            $.each(data.result.files, function (index, file) {
+                $('<p/>').text(file.name).appendTo('#files');
+            });
+        },
+        progressall: function (e, data) {
+            var progress = parseInt(data.loaded / data.total * 100, 10);
+            $('#progress .bar').css(
+                'width',
+                progress + '%'
+            );
+        }
+    });
+});
